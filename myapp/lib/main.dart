fcddf95e32cf8b2a6f8b84130ae903452ff4cc0b
Zerooxx001 <Doublezeroone505@gmail.com>
2026-05-29 14:22:23 -0300

    estructuracion de archivos

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/calculator_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,35 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mi App',
+      title: 'Calculadora',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Mi primera app")),
-      body: const Center(child: Text('Calculadora simple!')),
-      floatingActionButton: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-          backgroundColor: const Color.fromARGB(255, 18, 17, 19),
-          foregroundColor: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-        ),
-        child: Text('Presionar'),
-      ),
+      home: const CalculatorScreen(),
     );
   }
 }
